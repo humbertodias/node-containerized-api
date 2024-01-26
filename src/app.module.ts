2734fdea36bf85725b5fd5350fba108960c5d048
Humberto Dias <hldtux@gmail.com>
2024-01-26 09:00:42 -0300

    npx nest generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExamsModule } from './exams/exams.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [ExamsModule],
 })
 export class AppModule {}
